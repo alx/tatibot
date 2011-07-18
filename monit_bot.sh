0386bd951d5258d0e4656398612f1b970b5e3120
Alexandre Girard <git@alexgirard.com>
2011-07-18 18:05:48 +0200

    clean monit_bot.sh

--- monit_bot.sh
+++ monit_bot.sh
@@ -8,16 +8,13 @@ fi
 action=$1
 
 script_location=$(cd ${0%/*} && pwd -P)
-cd $script_location/..
 
-export RACK_ENV=production
-
-logfile=$script_location/../log/monit_job.log
+logfile=$script_location/monit_job.log
 echo "-----------------------------------------------" >> $logfile 2>&1
 echo "Running bundle exec ./bot/bot_control.rb $action" >> $logfile 2>&1
 echo `date` >> $logfile 2>&1
 echo `env` >> $logfile 2>&1
 
 /usr/local/bin/rvm reload >> $logfile 2>&1
-`which bundle` exec ./bot/bot_control.rb $action >> $logfile 2>&1
+`which bundle` exec ./bot_control.rb $action >> $logfile 2>&1
 
