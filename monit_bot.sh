9a4b1388d830a8c13bc9deb4664b43cd671707ab
Alexandre Girard <git@alexgirard.com>
2011-07-18 19:25:53 +0200

    cd

--- monit_bot.sh
+++ monit_bot.sh
@@ -10,13 +10,13 @@ action=$1
 script_location=$(cd ${0%/*} && pwd -P)
 
 logfile=$script_location/monit_job.log
+BUNDLE_GEMFILE=$script_location/Gemfile
 echo "-----------------------------------------------" >> $logfile 2>&1
-echo "Running bundle exec ./bot_control.rb $action" >> $logfile 2>&1
+echo "Running bundle exec $script_location//bot_control.rb $action" >> $logfile 2>&1
 echo `date` >> $logfile 2>&1
 echo `env` >> $logfile 2>&1
 
-BUNDLE_GEMFILE=$script_location/Gemfile
-
+cd $script_location
 /usr/local/bin/rvm reload >> $logfile 2>&1
 `which bundle` exec $script_location/bot_control.rb $action >> $logfile 2>&1
 
