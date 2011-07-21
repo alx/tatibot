5deb96ad862c2f40ff3950d6d6e2699f1b071853
Alexandre Girard <git@alexgirard.com>
2011-07-21 22:33:18 +0200

    correct score update

--- lib/nick_score.rb
+++ lib/nick_score.rb
@@ -3,17 +3,17 @@ class NickScore
 
   property :id,          Serial
   property :nick,        String
-  property :score,       Integer, :default => 1
+  property :score,       Integer, :default => 0
   property :created_at,  DateTime
 
   validates_presence_of :nick
 
   def increment
-    score += 1
+    self.score += 1
   end
 
   def decrement
-    score -= 1
+    self.score -= 1
   end
 end
 
